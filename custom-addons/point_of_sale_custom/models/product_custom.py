ad5ea288149ba6a858278984f4ee5f047d4c5f3b
AgusRC <1214370b@umich.mx>
2025-06-26 00:24:51 -0600

    campos autor y editorial en productos

--- custom-addons/point_of_sale_custom/models/product_custom.py
+++ custom-addons/point_of_sale_custom/models/product_custom.py
@@ -4,4 +4,7 @@ class ProductTemplateLibrary(models.Model):
     _inherit = 'product.template'
 
     # custom para libreria
+    autor = fields.Char(string='Autor', required=False)
+    editorial = fields.Char(string='Editorial', required=False)
     isbn = fields.Char(string='ISBN', required=False)
+    #TODO clave_sat
